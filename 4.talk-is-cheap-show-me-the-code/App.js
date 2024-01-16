d2d3882f9fdfc3ddeaa4676c4cbe537def0cb972
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-01-16 11:21:42 +0530

    On way

---
+++ 4.talk-is-cheap-show-me-the-code/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+
+const header=()=>{
+    return (
+        
+    )
+}
+
+const App=()=>{
+    <div className="app">
+        {/* header */}
+
+        {/* body */}
+
+        {/* footer */}
+
+    </div>
+}
+
+const root = document.ReactDOM(document.getElementById("root"));
+root.render("</>");
