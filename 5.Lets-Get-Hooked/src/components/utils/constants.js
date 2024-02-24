893f37400042c7472aed2200d15d631e43b8efff
ABDULHAMEED <skshaikabdulhameed786@gmail.com>
2024-02-24 19:10:38 +0530

    Added Button to filter Restaurants

--- 5.Lets-Get-Hooked/src/components/utils/constants.js
+++ 5.Lets-Get-Hooked/src/components/utils/constants.js
@@ -1,3 +1,4 @@
-const CDN="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"
+const CDN =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export default CDN;
